c4d3333fd53f0e7888387fe52d231c3c317c794d
Rane-19 <dia1929@yahoo.com.ph>
2024-03-08 16:17:28 -0800

    declaring again

--- script.js
+++ script.js
@@ -5,15 +5,13 @@
 // const start = getElementbyId('start-btn');
 // document.getElementById('timer-display').innerHTML = "Lourane";
 
-const start = document.querySelectorAll("start");
-const pause = document.querySelectorAll("pause");
-const reset = document.querySelectorAll("reset");
-
-
 // Timer Functions
 
-function startTimer (duration, display) {
-    document.getElementById('timer-display').innerHTML = duration;
-}
+// function startTimer (duration, display) {
+//     document.getElementById('timer-display').innerHTML = duration;
+// }
 
-startTimer(25:00, null);
+// startTimer(25:00, null);
+
+let timeLeft = 25 * 60; // Initial time set to 25 minutes
+let timerInterval;
